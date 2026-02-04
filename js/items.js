b837aa70a5ebc1fd71f10c31c997c994aec69dd3
Aurum2347 <119730649+Aurum2347@users.noreply.github.com>
2026-02-04 15:12:37 +0300

    Delete js directory

--- js/items.js
+++
@@ -1,6 +0,0 @@
-export const ITEMS = [
-  { id: 'white', name: 'Белый квадрат' },
-  { id: 'apple.png', name: 'Яблоко' },
-  { id: 'iron_ingot.png', name: 'Железный слиток' },
-  { id: 'stick.png', name: 'Палка' }
-];
